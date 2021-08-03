eb73a65eb7b11ac5299e55232f48036beb5cf305
avabrooks <72889462+avabrooks@users.noreply.github.com>
2021-08-03 16:26:27 -0700

    home page

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import (Flask, render_template)
+
+
+app = Flask(__name__)
+
+@app.route('/OurMission')
+def OurMission():
+    return render_template("mission.html")
+
+@app.route('/aboutus')
+def aboutus():
+    return render_template("aboutus.html")
+
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+
+if __name__ == "__main__":
+    app.run(port='3000', host='127.0.0.1')
+
